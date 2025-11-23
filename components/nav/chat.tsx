51fd0759505a029552ed5cb25d252b9ac3c56704
Ryan Kendrick <ryan.kendrick93@gmail.com>
2025-11-23 14:25:29 +1300

    fix duplicate welcome msg

--- components/nav/chat.tsx
+++ components/nav/chat.tsx
@@ -50,8 +50,7 @@ export default function Chat() {
     }
     mountedRef.current = true
     const now = Date.now()
-    setDisplayedMessages((prev) => [
-      ...prev,
+    setDisplayedMessages([
       {
         userId: "System-32." + now,
         name: "🖥️ System",
